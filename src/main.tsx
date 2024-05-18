95651edffdf3498a2d8804b28ff28ce19bd9a98f
Leorgino <leorgino2@gmail.com>
2024-05-18 15:19:50 +0200

    Agrega el codebase

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+
+import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, PokeDetail } from './pages';
+
+import "./index.scss"
+import PokemonProvider from './context/PokemonContext';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/:pokeId",
+    element: <PokeDetail />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <PokemonProvider>
+  <RouterProvider router={router} />
+  </PokemonProvider>
+  );
